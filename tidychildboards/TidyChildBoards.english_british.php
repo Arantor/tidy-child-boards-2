654d7591085549e8705afabd866a37ff51b8eb1a
Arantor <sleeping@myperch.org>
2016-05-12 20:43:54 +0100

    Port the language files from the old mod.

---
+++ tidychildboards/TidyChildBoards.english_british.php
@@ -0,0 +1,12 @@
+<?php
+
+$txt['tidy_child_boards'] = 'Arrange child boards in to columns';
+$txt['tidy_child_boards_no'] = 'Do not put in columns (default)';
+$txt['tidy_child_boards_2col'] = 'Two columns';
+$txt['tidy_child_boards_3col'] = 'Three columns';
+$txt['tidy_child_boards_4col'] = 'Four columns';
+$txt['tidy_child_boards_icon'] = 'Display small board icon next to child board';
+$txt['tidy_child_boards_bold'] = 'Display unread child boards in bold';
+$txt['tidy_child_boards_new'] = 'Display [new] image next to unread child boards';
+
+?>
---
+++ tidychildboards/TidyChildBoards.german.php
@@ -0,0 +1,12 @@
+<?php
+
+$txt['tidy_child_boards'] = 'Ordne Unterforen in Kolumnen an';
+$txt['tidy_child_boards_no'] = 'Keine Kolumnen verwenden (default)';
+$txt['tidy_child_boards_2col'] = 'Zwei Kolumnen';
+$txt['tidy_child_boards_3col'] = 'Drei Kolumnen';
+$txt['tidy_child_boards_4col'] = 'Vier Kolumnen';
+$txt['tidy_child_boards_icon'] = 'Zeige kleines Icon neben Unterforen';
+$txt['tidy_child_boards_bold'] = 'Hebe ungelesene Unterforen hervor';
+$txt['tidy_child_boards_new'] = 'Zeige [new] Icon neben ungelesenen Unterforen';
+
+?>
